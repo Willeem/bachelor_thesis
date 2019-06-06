224b5860e7f16220defbe27c0b8825a980cab4b3
Willeem <willem.datema@hotmail.com>
2019-06-06 18:51:18 +0200

    almost done

--- get_data.py
+++ get_data.py
@@ -6,11 +6,12 @@ from praw.models import MoreComments
 from collections import defaultdict
 from operator import itemgetter
 import os
-reddit = praw.Reddit(client_id='YpYx4r89UxeMag',
-                     client_secret='7nLiowFm1BmpNxmHlvirrEe8Hj8',
-                     user_agent='my user agent')
+reddit = praw.Reddit(client_id=os.environ.get('CLIENT_ID'),
+                     client_secret=os.environ.get('CLIENT_SECRET'),
+                     user_agent=os.environ.get('USER_ID'))
 
 def get_top_1000(authors,timefilter):
+    """Retrieves the top 1000 posts of the soccer subreddit from a given timefilter """
     for submission in reddit.subreddit('soccer').top(timefilter,limit=1000):
         for comment in submission.comments:
             if isinstance(comment,MoreComments):
@@ -19,6 +20,7 @@ def get_top_1000(authors,timefilter):
                 authors[comment.author_flair_text].add(comment.author)
     return authors
 def get_usernames():
+    """Gets the usernames needed and writes them to a pickle file"""
     authors = defaultdict(set)
     month_users = get_top_1000(authors,'month')
     extra_data = get_top_1000(month_users,'all')
@@ -27,6 +29,8 @@ def get_usernames():
 
 
 def clean_initial(authors):
+    """Cleans the name of the Reddit flairs and
+    combines some different spellings of a team into one class"""
     stripped_dict = defaultdict(set)
     for item in authors:
         stripped = item.strip(':')
@@ -40,6 +44,7 @@ def clean_initial(authors):
     return stripped_dict
 
 def count_initial(stripped_dict):
+    """Counts and sorts the amount of users per team"""
     count_dict = defaultdict(int)
     total_count = 0
     for item in stripped_dict:
@@ -50,6 +55,7 @@ def count_initial(stripped_dict):
     return sorted_authors
 
 def create_final_dict(sorted_authors,stripped_dict):
+    """Filters the teams represented by less than 100 users"""
     total = 0
     final_dict = defaultdict(set)
     for item in sorted_authors:
@@ -60,30 +66,30 @@ def create_final_dict(sorted_authors,stripped_dict):
     return final_dict
 
 def create_folders(clubs_with_authors):
+    """Creates a folder per team"""
     for item in clubs_with_authors:
         outpath = os.getcwd() + "/data/" + str(item)
         os.mkdir(outpath)
 
 def get_all_authors(final_dict):
+    """Retrieves and sorts all authors"""
     authors = [x.name for item in final_dict for x in final_dict[item]]
     return sorted(authors)
 
 
 def main():
-    #get_usernames()
-    with open('clubs_with_authors.pickle','rb') as f:
-        authors = pickle.load(f)
+    authors = get_usernames()
     stripped_dict = clean_initial(authors)
     sorted_authors = count_initial(stripped_dict)
     print(sorted_authors)
     final_dict = create_final_dict(sorted_authors,stripped_dict)
-    # all_authors_sorted = get_all_authors(final_dict)
-    # with open('final_dict.pickle','wb') as b:
-    #     pickle.dump(final_dict,b, protocol = pickle.HIGHEST_PROTOCOL)
-    # with open('authors.pickle','wb') as b:
-    #     pickle.dump(all_authors_sorted,b, protocol = pickle.HIGHEST_PROTOCOL)
+    all_authors_sorted = get_all_authors(final_dict)
+    with open('final_dict.pickle','wb') as b:
+        pickle.dump(final_dict,b, protocol = pickle.HIGHEST_PROTOCOL)
+    with open('authors.pickle','wb') as b:
+        pickle.dump(all_authors_sorted,b, protocol = pickle.HIGHEST_PROTOCOL)
 
-    #create_folders(final_dict)
+    create_folders(final_dict)
 
 if __name__ == "__main__":
     main()
